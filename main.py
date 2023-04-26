34a88a227f32f47ea98cf7fd2d535e553a3ae318
Singgih Cahyono <scahyono@gmail.com>
2023-04-26 14:49:26 +0200

    cache generated text and audio to save cost

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 elevenlabs.set_api_key(os.environ["ELEVENLABS_API_KEY"])
 
-# Ask the user for a topic
-topic = input("What is your topic? ")
+# Ask the user for a topic and convert it to lowercase
+topic = input("What is your topic? ").lower()
 
 # Set up the prompt limit to 50 words to save cost
 prompt = "Limit the result in maximum 50 words, generate a self hypnotherapy script I can listen to help me with " + topic + ".\n\n"
@@ -25,30 +25,43 @@ temperature = 0.7
 # The maximum number of tokens to generate
 max_tokens = 2048
 
-# Generate the completion
-response = openai.Completion.create(
-    engine=model_engine,
-    prompt=prompt,
-    temperature=temperature,
-    max_tokens=max_tokens,
-)
+# check if the text has been generated
+if os.path.isfile("out/" + topic + ".txt"):
+    print("The text has been generated. Generating audio...")
+    # Read the generated text
+    file = open("out/" + topic + ".txt", "r")
+    text = file.read()
+    file.close()
+else:
 
-# Text to be converted to audio
-# Concatenate all the responses
-text = "".join([choice.text for choice in response.choices])
+    # Generate the completion
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
+
+    # Text to be converted to audio
+    # Concatenate all the responses
+    text = "".join([choice.text for choice in response.choices])
+
+    # Save the generated script
+    file = open("out/" + topic + ".txt", "w")
+    file.write(text)
+    file.close()
 
 # Print the generated script
 print(text)
 print("Generating audio...")
 
-# Language code
-language = 'en'
+# check if the audio has not been generated
+if not os.path.isfile("out/" + topic + ".mp3"):
+    # Generate the audio and add extra spaces to prevent early cut off during playback
+    audio = elevenlabs.generate(text + " ... ...")
 
-# Generate the audio
-audio = elevenlabs.generate(text)
-
-# Save the audio file
-elevenlabs.save(audio, 'out/' + topic + '.mp3')
+    # Save the audio file
+    elevenlabs.save(audio, 'out/' + topic + '.mp3')
 
 # Play the audio file
 playsound('out/' + topic + '.mp3')
